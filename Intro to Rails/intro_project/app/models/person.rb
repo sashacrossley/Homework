e51a5be00942a922a4c5c987cfab112222b6f983
sashacrossley <sasha.m.crossley@gmail.com>
2020-11-25 15:28:47 +0000

    completed intro to rails

---
+++ Intro to Rails/intro_project/app/models/person.rb
@@ -0,0 +1,11 @@
+class Person < ApplicationRecord
+    validates :name, presence:true
+    # validates :house_id, presence:true
+
+    belongs_to(
+        :house,
+        class_name: 'House',
+        foreign_key: :house_id,
+        primary_key: :id
+    )
+end
